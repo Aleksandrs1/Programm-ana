952d5765372f4416f1362012cbe76e68adab1ffd
Aleksandrs1 <jels417@gmail.com>
2020-10-27 11:28:08 +0000

    Python

---
+++ strings.py
@@ -0,0 +1,10 @@
+va1 = 'Sveiki '
+print(va1)
+va2 = 'Pasaule'
+print(va1 , va2)
+#Teksta mainīgā lielums/ simbolu skaits teksta mainīgajā
+print(len(va1))
+print("va1[2:4]:", va1[::-1])
+print(va1*2)
+va2 = 'lfdgktl;rhjhfdgshe'
+print(va1,va2)
