8e665bc058aec5267f09af6d05ce9a69a033d6ba
Aleksandrs1 <jels417@gmail.com>
2020-10-27 11:41:23 +0000

    Darbs Pytona ar repozitoriju

---
+++ list.py
@@ -0,0 +1,13 @@
+my_list = [1,2,3]
+print(my_list, '\n')
+
+your_list = ['Teksts', 2,3.5]
+print(your_list, '\n')
+print("Lenght my_list:", len(my_list),'\n')
+
+print(your_list[0])
+print(your_list[0],'\n')
+my_list[0] = 'NOMAINĪJU'
+print(my_list, '\n')
+print(my_list + ["JAUNUMS"])
+print(my_list, '\n')
---
+++ numbers.py
@@ -0,0 +1,7 @@
+first = 2
+second = 2
+help = 4
+
+all = first + second + help
+
+print(all)
